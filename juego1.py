82f38367d800d917bc0fb0cd1cfd3ff98e7e71b9
a00831789 <77166759+a00831789@users.noreply.github.com>
2021-03-16 11:47:56 -0600

    Juego1.py 1
    se agrega la función de triangulo

--- juego1.py
+++ juego1.py
@@ -30,8 +30,17 @@ def rectangle(start, end):
     pass  # TODO
 
 def triangle(start, end):
-    "Draw triangle from start to end."
-    pass  # TODO
+"Draw triangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(4):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
+    
 
 def tap(x, y):
     "Store starting point or draw shape."
