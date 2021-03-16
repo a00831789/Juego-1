9ea86e3d9dc26f408369b7bcbe672e0e17c994cb
a00831789 <77166759+a00831789@users.noreply.github.com>
2021-03-16 13:21:47 -0600

    modificacion.py 3
    se agrega la función para hacer el círculo

--- modificacion.py
+++ modificacion.py
@@ -9,5 +9,20 @@ def triangle(start, end):
         forward(end.x - start.x)
         left(120)
     end_fill()
+    
+def circle_(start, end):
+    "Draw circle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    x_c=int(start.x)
+    y_c=int(start.y)
+    
+    for count in range(1):
+        circle(start.x-end.x)
+
+    end_fill()
 
 onkey(lambda: color('magenta'), 'M')
