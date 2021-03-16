a5a0047fe702713a2d1907bcbda37a437fca1b22
a00831789 <77166759+a00831789@users.noreply.github.com>
2021-03-16 11:46:18 -0600

    modificacion 1
    Se añade la función completa del triangulo

---
+++ modificacion.py
@@ -0,0 +1,11 @@
+def triangle(start, end):
+    "Draw triangle from start to end."
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(4):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
